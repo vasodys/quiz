5d143cefa4dc6001154676ee05c1f5b009d2ecd0
vasodys <vasilisodysseos@Vasiliss-MacBook-Pro.local>
2017-02-02 13:25:08 +0200

    Code Cleanup

--- functions.js
+++ functions.js
@@ -17,12 +17,12 @@ function getData() {
         method: "GET",
         url: "https://proto.io/en/jobs/candidate-questions/quiz.json"
     }).done(onDataReceived);
-   
     $.ajax({
         method: "GET",
         url: "https://proto.io/en/jobs/candidate-questions/result.json"
     }).done(onResultsReceived);
 }
+
 function onDataReceived(quizJson) {
     quiz = quizJson;
     console.log(quizJson);
@@ -30,46 +30,57 @@ function onDataReceived(quizJson) {
     showQuestion(quizJson.questions[currentQuestionPos]);
     $("#buttons").append("<button id='Submit' onclick='questionCheck()'>Submit</button>");
 }
+
 function onResultsReceived(resultsJson) {
     endResult = resultsJson;
     console.log(resultsJson);
 }
+
 function showQuestion(questionObj) {
     $("#questionTitle").text(questionObj.title);
     $("#questionImg").attr("src", questionObj.img);
     $("#possibleAnswers").empty();
     currentQuestion = questionObj;
+    showPossibleAns();
+    pushCorrectAns();
 
-    if (questionObj.possible_answers != null) {
-        switch (questionObj.question_type) {
-            case "mutiplechoice-single":
-                for (var i = 0; i < questionObj.possible_answers.length; i++) {
-                    labelId = "q-" + questionObj.q_id + "_" + "a-" + questionObj.possible_answers[i].a_id;
-                    $("#possibleAnswers").append("<label id='" + labelId + "'><li  class='list-group-item'><input type='radio' name='answer_value' value='" + questionObj.possible_answers[i].a_id + "'>" + questionObj.possible_answers[i].caption + "</li></label>");
-                }
-                break;
 
-            case "mutiplechoice-multiple":
-                for (var i = 0; i < questionObj.possible_answers.length; i++) {
-                    labelId = "q-" + questionObj.q_id + "_" + "a-" + questionObj.possible_answers[i].a_id;
-                    $("#possibleAnswers").append("<label id='" + labelId + "'><li  class='list-group-item'><input type='checkbox' name='answer_value'value='" + questionObj.possible_answers[i].a_id + "'>" + questionObj.possible_answers[i].caption + "</li></label>");
-                }
-                break;
+}
 
-        }
-
-    } else {
-        labelId = "q-" + questionObj.q_id + "_" + "a-";
-        $("#possibleAnswers").append("<label id='" + labelId + "true'><li class='list-group-item'><input type='radio' name='answer_value' value='true'>True</li></label><label id='" + labelId + "false'><li  class='list-group-item'><input type='radio' name='answer_value' value='false'>False</li></label");
-    }
+function pushCorrectAns(){
     if( currentQuestion.question_type == "mutiplechoice-multiple") {
         for (var i = 0; i < currentQuestion.correct_answer.length; i++) {
-            correctAnswer.push("q-" + questionObj.q_id + "_" + "a-" + currentQuestion.correct_answer[i]);
+            correctAnswer.push("q-" + currentQuestion.q_id + "_" + "a-" + currentQuestion.correct_answer[i]);
         }
     } else {
-        correctAnswer.push("q-" + questionObj.q_id + "_" + "a-" + currentQuestion.correct_answer);
+        correctAnswer.push("q-" + currentQuestion.q_id + "_" + "a-" + currentQuestion.correct_answer);
     }
 }
+
+function showPossibleAns(){
+    if (currentQuestion.possible_answers == null) {
+        labelId = "q-" + currentQuestion.q_id + "_" + "a-";
+        $("#possibleAnswers").append("<label id='" + labelId + "true'><li class='list-group-item'>" +
+            "<input type='radio' name='answer_value' value='true'>True</li></label><label id='" + labelId + "false'>" +
+            "<li  class='list-group-item'><input type='radio' name='answer_value' value='false'>False</li></label>");
+
+    } else {
+        var inputType;
+        if(currentQuestion.question_type == "mutiplechoice-multiple") {
+             inputType = "checkbox";
+        } else {
+             inputType = "radio";
+        }
+
+        for (var i = 0; i < currentQuestion.possible_answers.length; i++) {
+            labelId = "q-" + currentQuestion.q_id + "_" + "a-" + currentQuestion.possible_answers[i].a_id;
+            $("#possibleAnswers").append("<label id='" + labelId + "'><li  class='list-group-item'>" +
+                "<input type='" + inputType + "' name='answer_value' value='" + currentQuestion.possible_answers[i].a_id + "'>" + currentQuestion.possible_answers[i].caption + "</li></label>");
+        }
+
+    }
+}
+
 
 function questionCheck() {
     $("#Submit").prop('disabled', true);
@@ -129,26 +140,23 @@ function nextQuestion() {
         currentQuestionPos++;
         showQuestion(quiz.questions[currentQuestionPos]);
     } else {
-        $("#possibleAnswers").empty();
-        $("#buttons").empty();
-        $("#questionImg").attr("src", "");
+        clearQuestion();
         endQuiz();
     }
 }
-function prevQuestion() {
-    if (currentQuestionPos > 0) {
-        $("#quetion").empty();
-        $("#questionImg").attr("src","");
-        currentQuestionPos--;
-        showQuestion(quiz.questions[currentQuestionPos]);
-    }
+
+function clearQuestion(){
+    $("#possibleAnswers").empty();
+    $("#buttons").empty();
+    $("#questionImg").attr("src", "");
 }
+
 function endQuiz() {
-    for (j=0; j< quiz.questions.length; j++){
+    for (var j=0; j< quiz.questions.length; j++){
         maxScore += quiz.questions[j].points;
     }
     var percentage = (score/maxScore)*100;
-    for (i = 0; i < endResult.results.length; i++){
+    for (var i = 0; i < endResult.results.length; i++){
         if (percentage >= endResult.results[i].minpoints && percentage <= endResult.results[i].maxpoints){
             $("#quizTitle").text(endResult.results[i].title);
             $("#scoreTitle").text("Your score is " + Math.round(percentage) + "%");
